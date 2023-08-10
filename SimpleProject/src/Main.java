6ec7a6091a9c7a0d99255be020c78d26e38aecd7
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-10 19:09:54 +0200

    feat : add Scanner in Main.java

--- SimpleProject/src/Main.java
+++ SimpleProject/src/Main.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 /**
- * 8/10/2023
- * SimpleProject
+ * 8/10/2023 SimpleProject
  *
  * @author Marsel Sidikov (AIT TR)
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Start project...");
-    }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Start project...");
+  }
 }
