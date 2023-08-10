4cc355d1f4dfa1c7c7de7445463fdc9d0a9adc1f
Marsel Sidikov <sidikov.marsel@gmail.com>
2023-08-10 19:13:47 +0200

    add a,b and c as sum of their

--- SimpleProject/src/Main.java
+++ SimpleProject/src/Main.java
@@ -9,6 +9,11 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Start project...");
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = a + b;
+
+    System.out.println(c);
   }
 }
