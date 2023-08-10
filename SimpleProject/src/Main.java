e3992c6b0ef74db758109656c91ddd78e50d1cdd
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-10 19:18:04 +0200

    feat : add Strings a1 a2

--- SimpleProject/src/Main.java
+++ SimpleProject/src/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Start project...");
+
+    String a1 = sc.nextLine();
+    String a2 = sc.nextLine();
+    System.out.println(a1 + a2);
   }
 }
