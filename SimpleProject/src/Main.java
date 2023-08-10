270a67169914fb85fdc1fab9eaccf608a0308c7c
Marsel Sidikov <sidikov.marsel@gmail.com>
2023-08-10 19:05:13 +0200

    add Main.java with simple console message

---
+++ SimpleProject/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * 8/10/2023
+ * SimpleProject
+ *
+ * @author Marsel Sidikov (AIT TR)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start project...");
+    }
+}
